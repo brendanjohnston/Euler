e402a2a1b790ae5d743240481e4d9d325039636d
brendanjohnston <brendan@johnston.net.au>
2014-06-10 22:30:10 +0800

    My first foray into Clojure

---
+++ 001/001.v1.clj
@@ -0,0 +1,23 @@
+
+;; my very first foray into clojure
+(defn p001
+  [multiples limit]
+  (reduce
+    (fn
+      [sum, value]
+      (if
+        (first
+          (filter
+            #(= 0 (mod value %))
+            multiples
+          )
+        )
+        (+ sum value)
+        sum)
+    )
+    (range 0 limit)
+  )
+)
+
+(p001 [3 5] 1000)
+
