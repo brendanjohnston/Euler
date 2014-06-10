b5aa7b8b7c8e84b2d0e61c7a2402c6607ffc6fc4
brendanjohnston <brendan@johnston.net.au>
2014-06-11 00:08:07 +0800

    While loops in Clojure ... not so glamorous

---
+++ 002/002.v2.clj
@@ -0,0 +1,21 @@
+(defn p002
+  [limit]
+  (def x 0)
+  (def y 1)
+  (def s 0)
+  (while
+    (< y limit)
+    (do
+      (def z (+ x y))
+      (def x y)
+      (def y z)
+      (if
+        (even? x)
+        (def s (+ s x))
+      )
+    )
+  )
+  (identity s)
+)
+
+(println (str "solution: " (p002 4000000)))
