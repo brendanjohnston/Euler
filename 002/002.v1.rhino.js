07d185295b67ba0211e792cf0e6b47a73620e179
brendanjohnston <brendan@johnston.net.au>
2014-06-11 20:55:52 +0800

    OCD

--- 002/002.v1.rhino.js
+++ 002/002.v1.rhino.js
@@ -1,19 +1,18 @@
-function p002 (limit)
-{
+function p002 (limit) {
+
     var sum = 0;
 
     var x = 0,
         y = 1,
         z;
 
-    while (y < limit)
-    {
+    while (y < limit) {
         z = x + y;
         x = y;
         y = z;
 
-        if (x & 1 == 1) //if (x % 2 == 0)
-        {
+        if (x & 1 == 1) {
+
             sum += x;
         }
     }
