a929f675ed424860026b2a2573ebb918b09f814e
brendanjohnston <brendan@johnston.net.au>
2014-06-12 22:33:22 +0800

    Damn you IntelliJ.

--- 003/003.v1.mysql.sql
+++ 003/003.v1.mysql.sql
@@ -6,27 +6,31 @@ DELIMITER $$
 CREATE PROCEDURE `sp_003` (IN `limit` INTEGER)
 BEGIN
 
+    DECLARE `inner_limit` INTEGER;
+
+    SET `inner_limit` = CEIL(SQRT(`limit`));
+
     SELECT
         MAX(`value`) as `solution`
     FROM
     `tbl_integers`
     WHERE
-        `value` BETWEEN 2 AND `limit`
+        `value` BETWEEN 2 AND `inner_limit`
         AND
         MOD(`limit`, `value`) = 0
         AND
         `value` NOT IN (
             SELECT
-                i1.`value` * i2.`value`
+                i1.`value` * i2.`value` as `non_prime`
             FROM
             `tbl_integers` AS i1
             INNER JOIN `tbl_integers` AS i2
                 ON
                     i1.`value` >= i2.`value`
                     AND
-                    i2.`value` BETWEEN 2 AND (`limit` / i1.`value`)
+                    i2.`value` BETWEEN 2 AND (`inner_limit` / i1.`value`)
             WHERE
-                i1.`value` BETWEEN 2 AND (`limit` / 2)
+                i1.`value` BETWEEN 2 AND `inner_limit`
     );
 
 END
