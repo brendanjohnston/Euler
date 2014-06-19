d715f064daccac2895bf979ca516dad5913e5042
brendanjohnston <brendan@johnston.net.au>
2014-06-19 21:51:57 +0800

    Tweaked indenting

--- 003/003.v1.clj
+++ 003/003.v1.clj
@@ -11,20 +11,13 @@
           (if
             (some
               #(zero? (mod value %))
-              (take-while
-                #(< % inner_value)
-                primes
-              )
+              (take-while #(< % inner_value) primes)
             )
             primes
             (conj primes value)
-          )
-        )
+          ))
         (into [[2]] (range 3 inner_limit 2))
-      )
-    )
-  )
-)
+      ))))
 
 (defn now [] (java.util.Date.))
 
